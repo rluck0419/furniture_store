48d0041993ca837f2e63380fa45f98407ef6d274
Robert Luckfield <rluck0419@gmail.com>
2016-07-11 15:35:22 -0500

    html better formatted for results - displays list price, current price (discounted or not), condition, and quantity

--- furniture_store/app/models/product.rb
+++ furniture_store/app/models/product.rb
@@ -4,4 +4,12 @@ class Product < ApplicationRecord
   validates :condition, presence: true
   validates :on_clearance, inclusion: { in: [true, false] }
   validates :quantity, numericality: { only_integer: true }
+
+  def display_price
+    if on_clearance
+      self.price *= 0.9 if condition.downcase == 'good'
+      self.price *= 0.8 if condition.downcase == 'average'
+    end
+    self.price
+  end
 end
