0465e290a6a095795a2c6acb07bae21336e7cfbb
Robert Luckfield <rluck0419@gmail.com>
2016-07-11 14:29:37 -0500

    controller & routes for show/index

--- furniture_store/app/controllers/products_controller.rb
+++ furniture_store/app/controllers/products_controller.rb
@@ -1,7 +1,13 @@
 class ProductsController < ApplicationController
   def index
+    render template: 'products/index.html.erb', locals: { products: Product.all }
   end
 
   def show
+    if Product.exists?(params[:id])
+      render template: 'products/show.html.erb', locals: { product: Product.find(params[:id]) }
+    else
+      render html: 'Product not found', status: 404
+    end
   end
 end
