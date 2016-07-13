06936c6666ce2b0637eb367029988934b7776a98
Robert Luckfield <rluck0419@gmail.com>
2016-07-12 21:42:38 -0500

    completion of explorer mode

--- furniture_store/app/controllers/products_controller.rb
+++ furniture_store/app/controllers/products_controller.rb
@@ -3,6 +3,56 @@ class ProductsController < ApplicationController
     render template: 'products/index.html.erb', locals: { products: Product.all }
   end
 
+  def index_automotive
+    products = []
+    Product.all.each do |product|
+      if product.category.category_name == 'Automotive'
+        products << product
+      end
+    end
+    render template: 'products/category_index.html.erb', locals: { products: products }
+  end
+
+  def index_electronics
+    products = []
+    Product.all.each do |product|
+      if product.category.category_name == 'Electronics'
+        products << product
+      end
+    end
+    render template: 'products/category_index.html.erb', locals: { products: products }
+  end
+
+  def index_industrial
+    products = []
+    Product.all.each do |product|
+      if product.category.category_name == 'Industrial'
+        products << product
+      end
+    end
+    render template: 'products/category_index.html.erb', locals: { products: products }
+  end
+
+  def index_tools
+    products = []
+    Product.all.each do |product|
+      if product.category.category_name == 'Tools'
+        products << product
+      end
+    end
+    render template: 'products/category_index.html.erb', locals: { products: products }
+  end
+
+  def index_music
+    products = []
+    Product.all.each do |product|
+      if product.category.category_name == 'Music'
+        products << product
+      end
+    end
+    render template: 'products/category_index.html.erb', locals: { products: products }
+  end
+
   def show
     if Product.exists?(params[:id])
       render template: 'products/show.html.erb', locals: { product: Product.find(params[:id]) }
--- furniture_store/config/routes.rb
+++ furniture_store/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/products',      to: 'products#index'
-  get '/products/:id',  to: 'products#show'
+  get '/products',              to: 'products#index'
+  get '/products/Automotive',   to: 'products#index_automotive'
+  get '/products/Electronics',  to: 'products#index_electronics'
+  get '/products/Industrial',   to: 'products#index_industrial'
+  get '/products/Tools',        to: 'products#index_tools'
+  get '/products/Music',        to: 'products#index_music'
+  get '/products/:id',          to: 'products#show'
 
   root 'dashboard#index'
 end
