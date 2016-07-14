2b8a3d3e8f4509aad091a2637b91dd794eacf3e3
Robert Luckfield <rluck0419@gmail.com>
2016-07-13 22:46:00 -0500

    created html for edit/new and _form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,37 @@ class ProductsController < ApplicationController
       render html: 'Product not found', status: 404
     end
   end
+
+  def new
+    render locals: {
+      product: Product.new
+    }
+  end
+
+  def create
+    product = Product.new
+    product.name = params[:name]
+    product.price = params[:price]
+    product.picture = Faker::Avatar.image
+    product.condition = params[:condition]
+    product.on_clearance = params[:on_clearance]
+    product.quantity = params[:quantity]
+    product.category_id = params[:category_id]
+  end
+
+  def edit
+    render locals: {
+      product: Product.find(params[:id])
+    }
+  end
+
+  def update
+    product = Product.find(params[:id])
+    product.name = params[:name]
+    product.price = params[:price]
+    product.condition = params[:condition]
+    product.on_clearance = params[:on_clearance]
+    product.quantity = params[:quantity]
+    product.category_id = params[:category_id]
+  end
 end
