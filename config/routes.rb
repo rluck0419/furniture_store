040b2e538f09a63e957ed220ccaf28a48d031827
Robert Luckfield <rluck0419@gmail.com>
2016-07-13 22:26:38 -0500

    edited/created routes - for edit/create

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/products',            to: 'products#index'
-  # get '/products/Industrial', to: 'products#index_industrial'
-  # get '/products/Shoes',      to: 'products#index_shoes'
-  # get '/products/Beauty',     to: 'products#index_beauty'
-  # get '/products/Clothing',   to: 'products#index_clothing'
-  # get '/products/Tools',      to: 'products#index_tools'
-  get '/products/:id',        to: 'products#show'
+  get 'products',       to: 'products#index'
+  get 'products/:id',   to: 'products#show'
+  post 'products',      to: 'products#create'
+  put 'products/:id',   to: 'products#update'
 
-  get '/categories',          to: 'categories#index'
-  get '/categories/:id',      to: 'categories#show'
+  get 'categories',     to: 'categories#index'
+  get 'categories/:id', to: 'categories#show'
 
   root 'dashboard#index'
 end
