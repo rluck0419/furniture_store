df7edf3336063e714744cd8d39ec34be15a4329f
Robert Luckfield <rluck0419@gmail.com>
2016-07-13 23:12:38 -0500

    basic form for create/edit - not currently working

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'products',       to: 'products#index'
-  get 'products/:id',   to: 'products#show'
-  post 'products',      to: 'products#create'
-  put 'products/:id',   to: 'products#update'
+  get 'products',           to: 'products#index'
+  get 'products/new',       to: 'products#new'
+  get 'products/:id',       to: 'products#show'
+  get 'products/:id/edit',  to: 'products#edit'
+  post 'products',          to: 'products#create'
+  put 'products/:id',       to: 'products#update'
 
-  get 'categories',     to: 'categories#index'
-  get 'categories/:id', to: 'categories#show'
+  get 'categories',         to: 'categories#index'
+  get 'categories/:id',     to: 'categories#show'
 
   root 'dashboard#index'
 end
